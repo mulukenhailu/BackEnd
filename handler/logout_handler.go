932535a6a0fc2a4ca512d6d0a43e86f5df6b086c
Muluken Hailu <MulukenHailu@Muluken-hp-pavilion.local>
2023-10-03 19:01:00 -0700

    new commit

--- handler/logout_handler.go
+++ handler/logout_handler.go
@@ -7,14 +7,14 @@ import (
 )
 
 func (ph *profileHandler) Logout(c *gin.Context) {
-	metadata, _ := ph.tk.ExtractedMetaData(c.Request)
+	// metadata, _ := ph.tk.ExtractedMetaData(c.Request)
 
-	if metadata != nil {
-		deleteErr := ph.rd.DeleteTokens(metadata)
-		if deleteErr != nil {
-			c.JSON(http.StatusBadRequest, deleteErr.Error())
-			return
-		}
-	}
+	// if metadata != nil {
+	// 	deleteErr := ph.rd.DeleteTokens(metadata)
+	// 	if deleteErr != nil {
+	// 		c.JSON(http.StatusBadRequest, deleteErr.Error())
+	// 		return
+	// 	}
+	// }
 	c.JSON(http.StatusOK, "Successfully log out!")
 }
